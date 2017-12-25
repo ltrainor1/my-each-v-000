9867ca1919aeb9ead5dd2397d0ee64eb0e37ea7c
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-25 07:30:10 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,14 @@
 
 
 def my_each(array)
+  
   collection = [1,2,3,4]
   i = 0 
   while i < collection.length 
     yield(array[i])
     i += 1 
   end 
+  array 
 end
 
 my_each([1,2,3,4]) do |i|
